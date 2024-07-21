9b9cd8d636dc0a365ee8b7e3ca7d5888fe6724dd
edgarlawrence <lawofueki91@hotmail.com>
2024-07-22 01:19:36 +0700

    Initial Commit

---
+++ src/test/java/Ecommerce/Initial_Project/InitialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecommerce.Initial_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
